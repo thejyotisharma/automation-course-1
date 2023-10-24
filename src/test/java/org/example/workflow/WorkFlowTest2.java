86210af7dd3bafabee0cc3cdddcef6a0226b03a7
thejyotisharma <jyotisharma.js3031@gmail.com>
2023-10-24 21:36:42 +0530

    tests

---
+++ src/test/java/org/example/workflow/WorkFlowTest2.java
@@ -0,0 +1,11 @@
+package org.example.workflow;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WorkFlowTest2 {
+
+    @org.junit.jupiter.api.Test
+    void run() throws InterruptedException {
+        WorkFlow.run();
+    }
+}
